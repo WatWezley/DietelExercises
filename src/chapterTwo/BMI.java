2d278ce8ae53b2d2db3a18c6f04141b7df2aec6d
Nwokonko Ugochi <magwatwezley@gmail.com>
2022-11-19 18:03:14 +0100

    Added older chibuzo's troubles

---
+++ src/chapterTwo/BMI.java
@@ -0,0 +1,34 @@
+package chapterTwo;input java.util.Scanner;
+
+	publi class BMI{
+
+	public static void main(String[] args);
+
+	Scanner input = newScanner(System.in);
+	
+	System.out.print("Enter weightInKg : ";
+	
+	int WeightInkg = input.nextInt() ; 
+
+	System.out.print("Enter heightInKg : ";
+	
+	int heightInkg = input.nextInt() ; 
+
+	double BMI = weightInKg/heightInkg * heightInkg) ;
+
+
+	If( BMI < 18.5 ) {
+		System.out.println("Under Weight") ;
+
+	}
+
+	if ( BMI > 18.5 && BMI > 25) {
+		System.out.println("Under Weight") ;
+	} 
+
+	if ( BMI > 25 && BMI > 3) {
+		System.out.println("Under Weight") ;
+	} 
+
+
+}
