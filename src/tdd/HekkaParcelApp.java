760f8111fb263e8b5bd73f112f37538117cceba3
Nwokonko Ugochi <magwatwezley@gmail.com>
2022-11-21 00:08:29 +0100

    updated chibuzor trouble

--- src/tdd/HekkaParcelApp.java
+++ src/tdd/HekkaParcelApp.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class HekkaParcelApp {
 
     public static void main(String[] args) {
-        HekkaParcel ugo = new HekkaParcel(0,0);
+
+        HekkaParcel ugo = new HekkaParcel(0, 0);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print(" Enter amount of Parcel Scanned:");
+        System.out.print("Enter amount of Parcel Scanned:");
         int parcelScanned = scanner.nextInt();
         ugo.setParcelScanned(parcelScanned);
 
@@ -16,8 +17,25 @@ public class HekkaParcelApp {
         int parcelDelivered = scanner.nextInt();
         ugo.setParcelDelivered(parcelDelivered);
 
-        System.out.printf("The Collection Rate is %.2f%n", ugo.getCollectionRate(parcelScanned,parcelDelivered));
-        System.out.printf("The Commission on Delivery is %.2f%n", ugo.getBonus(ugo.getCollectionRate(parcelScanned,parcelDelivered)));
+        while (parcelDelivered < 0 || parcelScanned < 0) {
+            System.out.println("OGA ENTER THE RIGHT INFORMATION JARE");
+            System.out.println();
+            System.out.print("Enter amount of Parcel Scanned:");
+            int anotherParcelScanned = scanner.nextInt();
+            ugo.setParcelScanned(anotherParcelScanned);
+
+            System.out.print("Enter amount of Parcel Delivered:");
+            int anotherParcelDelivered = scanner.nextInt();
+            ugo.setParcelDelivered(anotherParcelDelivered);
+
+            System.out.println();
+            System.out.printf("The Collection Rate is %.2f%n", ugo.getCollectionRate(anotherParcelScanned, anotherParcelDelivered));
+            System.out.printf("The Commission on Delivery is %.2f%n", ugo.getBonus(ugo.getCollectionRate(anotherParcelScanned, anotherParcelDelivered)));
+            break;}
+
+        System.out.println();
+        System.out.printf("The Collection Rate is %.2f%n", ugo.getCollectionRate(parcelScanned, parcelDelivered));
+        System.out.printf("The Commission on Delivery is %.2f%n", ugo.getBonus(ugo.getCollectionRate(parcelScanned, parcelDelivered)));
 
 
     }
